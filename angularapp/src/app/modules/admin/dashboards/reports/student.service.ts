5c5803541374b3ff1c9f669028b4dde925648152
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2023-09-04 21:04:39 -0500

    last commit

--- angularapp/src/app/modules/admin/dashboards/reports/student.service.ts
+++ angularapp/src/app/modules/admin/dashboards/reports/student.service.ts
@@ -35,6 +35,7 @@ export class StudentService {
                             name: `${nombres} ${apellidoPaterno} ${apellidoMaterno}`,
                         };
                     });
+                    
                     this.students = students;
                     return students;
                 })
