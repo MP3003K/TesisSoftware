0c6488a9bd722160d39819cda7b91d119142605e
Jezer Razuri <jezerrazuri@upeu.edu.pe>
2023-10-11 21:56:22 -0500

    last commit

---
+++ angularapp/src/app/modules/admin/dashboards/reports/excel.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { Workbook } from "exceljs"
+import fs from "file-saver"
+@Injectable({
+    providedIn: "root"
+})
+export class ExcelService {
+    private _workbook: Workbook
+    constructor() { }
+    async downloadReport() {
+        this._workbook = new Workbook()
+        this._workbook.creator = 'Evaluator'
+
+        const data = await this._workbook.xlsx.writeBuffer()
+        const blob = new Blob([data])
+        fs.saveAs(blob, "report.xlsx")
+    }
+
+
+}
