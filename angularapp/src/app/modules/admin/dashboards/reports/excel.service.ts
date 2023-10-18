f21d86f0351606e7f0ea142dfdc1cda08e252687
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2023-10-18 09:22:56 -0500

    excel commit

--- angularapp/src/app/modules/admin/dashboards/reports/excel.service.ts
+++ angularapp/src/app/modules/admin/dashboards/reports/excel.service.ts
@@ -7,12 +7,33 @@ import fs from "file-saver"
 export class ExcelService {
     private _workbook: Workbook
     constructor() { }
-    async downloadReport() {
+    async downloadReport(excel_data) {
         this._workbook = new Workbook()
         this._workbook.creator = 'Evaluator'
-
+        this._createTable(excel_data)
+        this._createDetail(excel_data)
         const data = await this._workbook.xlsx.writeBuffer()
         const blob = new Blob([data])
         fs.saveAs(blob, "report.xlsx")
+    }
+
+    async _createTable(data) {
+        const sheet = this._workbook.addWorksheet("Reporte")
+
+        sheet.mergeCells("B2:F2")
+        const hse_title = sheet.getCell("B2")
+        hse_title.value = 'Escala HSE'
+        hse_title.alignment = { horizontal: 'center', vertical: 'middle' };
+
+        sheet.mergeCells("H2:L2")
+        const fr_title = sheet.getCell("H2")
+        fr_title.value = 'Escala FR'
+        fr_title.alignment = { horizontal: 'center', vertical: 'middle' };
+
+
+    }
+
+    async _createDetail(data) {
+
     }
 }
