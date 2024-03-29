9b42f1b8bb9554025aeee61b4cff7ffd349659eb
Jezer <jezerrazuri@upeu.edu.pe>
2024-03-28 19:25:12 -0500

    last commit

--- angular.Server/Controllers/AuthController.cs
+++ angular.Server/Controllers/AuthController.cs
@@ -1,15 +1,10 @@
-﻿
-
-using Application.Wrappers;
-using Azure;
-using Context;
+﻿using Context;
 using Dapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.Data;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
 
