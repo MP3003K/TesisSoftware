f012d845a84fd57605c205488f8d42d656711595
Jezer <jezerrazuri@upeu.edu.pe>
2024-05-21 23:12:35 -0500

    last commit

--- angular.client/src/app/app.routes.ts
+++ angular.client/src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { initialDataResolver } from 'app/app.resolvers';
 import { AuthGuard } from 'app/core/auth/guards/auth.guard';
 import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
 import { LayoutComponent } from 'app/layout/layout.component';
+import { RoleGuard } from './core/auth/guards/role.guard';
 
 // @formatter:off
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const appRoutes: Route[] = [
-
     // Redirect empty path to '/example'
     { path: '', pathMatch: 'full', redirectTo: 'home' },
 
@@ -26,15 +26,41 @@ export const appRoutes: Route[] = [
         canActivateChild: [NoAuthGuard],
         component: LayoutComponent,
         data: {
-            layout: 'empty'
+            layout: 'empty',
         },
         children: [
-            { path: 'confirmation-required', loadChildren: () => import('app/modules/auth/confirmation-required/confirmation-required.routes') },
-            { path: 'forgot-password', loadChildren: () => import('app/modules/auth/forgot-password/forgot-password.routes') },
-            { path: 'reset-password', loadChildren: () => import('app/modules/auth/reset-password/reset-password.routes') },
-            { path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.routes') },
-            { path: 'sign-up', loadChildren: () => import('app/modules/auth/sign-up/sign-up.routes') }
-        ]
+            {
+                path: 'confirmation-required',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/confirmation-required/confirmation-required.routes'
+                    ),
+            },
+            {
+                path: 'forgot-password',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/forgot-password/forgot-password.routes'
+                    ),
+            },
+            {
+                path: 'reset-password',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/reset-password/reset-password.routes'
+                    ),
+            },
+            {
+                path: 'sign-in',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-in/sign-in.routes'),
+            },
+            {
+                path: 'sign-up',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-up/sign-up.routes'),
+            },
+        ],
     },
 
     // Auth routes for authenticated users
@@ -44,27 +70,59 @@ export const appRoutes: Route[] = [
         canActivateChild: [AuthGuard],
         component: LayoutComponent,
         data: {
-            layout: 'empty'
+            layout: 'empty',
         },
         children: [
-            { path: 'sign-out', loadChildren: () => import('app/modules/auth/sign-out/sign-out.routes') },
-            { path: 'unlock-session', loadChildren: () => import('app/modules/auth/unlock-session/unlock-session.routes') }
-        ]
+            {
+                path: 'sign-out',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-out/sign-out.routes'),
+            },
+            {
+                path: 'unlock-session',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/unlock-session/unlock-session.routes'
+                    ),
+            },
+        ],
     },
     {
         path: '',
-        canActivate: [AuthGuard],
-        canActivateChild: [AuthGuard],
+        canActivate: [AuthGuard, RoleGuard],
+        canActivateChild: [AuthGuard, RoleGuard],
         component: LayoutComponent,
         resolve: {
-            initialData: initialDataResolver
+            initialData: initialDataResolver,
         },
         children: [
-            { path: 'home', loadChildren: () => import('app/modules/admin/home/home.routes') },
-            { path: 'evaluation', loadChildren: () => import('app/modules/admin/evaluation/evaluation.routes') },
-            { path: 'reports', loadChildren: () => import('app/modules/admin/reports/reports.routes') },
-            { path: 'classrooms', loadChildren: () => import('app/modules/admin/classrooms/classrooms.routes') },
-            { path: 'questionaries', loadChildren: () => import('app/modules/admin/questionaries/questionaries.routes') },
+            {
+                path: 'home',
+                loadChildren: () =>
+                    import('app/modules/admin/home/home.routes'),
+            },
+            {
+                path: 'evaluation',
+                loadChildren: () =>
+                    import('app/modules/admin/evaluation/evaluation.routes'),
+            },
+            {
+                path: 'reports',
+                loadChildren: () =>
+                    import('app/modules/admin/reports/reports.routes'),
+            },
+            {
+                path: 'classrooms',
+                loadChildren: () =>
+                    import('app/modules/admin/classrooms/classrooms.routes'),
+            },
+            {
+                path: 'questionaries',
+                loadChildren: () =>
+                    import(
+                        'app/modules/admin/questionaries/questionaries.routes'
+                    ),
+            },
 
             {
                 path: '404-not-found',
@@ -75,6 +133,6 @@ export const appRoutes: Route[] = [
                     ),
             },
             { path: '**', redirectTo: '404-not-found' },
-        ]
-    }
+        ],
+    },
 ];
--- angular.client/src/app/modules/admin/home/home.component.ts
+++ angular.client/src/app/modules/admin/home/home.component.ts
@@ -1,17 +1,47 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+    ChangeDetectorRef,
+    Component,
+    OnDestroy,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { UserService } from 'app/core/user/user.service';
+import { User } from 'app/core/user/user.types';
+import { SharedModule } from 'app/shared/shared.module';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
-    selector     : 'app-home',
-    standalone   : true,
-    templateUrl  : './home.component.html',
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
     encapsulation: ViewEncapsulation.None,
+    imports: [MatCardModule, SharedModule],
 })
-export class HomeComponent
-{
+export class HomeComponent implements OnInit, OnDestroy {
+    user: User;
+
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
     /**
      * Constructor
      */
-    constructor()
-    {
+    constructor(
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _service: UserService
+    ) {}
+    ngOnInit(): void {
+        this._service.user$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((user: User) => {
+                this.user = user;
+
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            });
+    }
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
     }
 }
